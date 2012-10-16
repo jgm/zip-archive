34fd665798b68365faa56352116ca52b7891c089
John MacFarlane <fiddlosopher@gmail.com>
2012-10-16 10:13:18 -0700

    Added proper cabal test suite.

--- tests/test-zip-archive.hs
+++ tests/test-zip-archive.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 -- Test suite for Codec.Archive.Zip
 -- runghc Test.hs
 
@@ -5,10 +6,13 @@ import Codec.Archive.Zip
 import System.Directory
 import Test.HUnit.Base
 import Test.HUnit.Text
+#if MIN_VERSION_directory(1,2,0)
 import System.Time
+#endif
 import System.Process
 import qualified Data.ByteString.Lazy as B
 import Control.Applicative
+import System.Exit
 
 -- define equality for Archives so timestamps aren't distinguished if they
 -- correspond to the same MSDOS datetime.
@@ -21,18 +25,20 @@ instance Eq Archive where
 main :: IO Counts
 main = do
   createDirectory "test-temp"
-  counts <- runTestTT $ TestList [ 
-                                   testReadWriteArchive
-                                 , testReadExternalZip
-                                 , testFromToArchive
-                                 , testReadWriteEntry
-                                 , testAddFilesOptions
-                                 , testDeleteEntries
-                                 , testExtractFiles
-                                 ]
+  res   <- runTestTT $ TestList [ testReadWriteArchive
+                                , testReadExternalZip
+                                , testFromToArchive
+                                , testReadWriteEntry
+                                , testAddFilesOptions
+                                , testDeleteEntries
+                                , testExtractFiles
+                                ]
   removeDirectoryRecursive "test-temp"
-  return counts
+  exitWith $ case errors res of
+                     0 -> ExitSuccess
+                     n -> ExitFailure n
 
+testReadWriteArchive :: Test
 testReadWriteArchive = TestCase $ do
   archive <- addFilesToArchive [OptRecursive] emptyArchive ["LICENSE", "Codec"]
   B.writeFile "test-temp/test1.zip" $ fromArchive archive
@@ -40,8 +46,9 @@ testReadWriteArchive = TestCase $ do
   assertEqual "for writing and reading test1.zip" archive archive'
   assertEqual "for writing and reading test1.zip" archive archive'
 
+testReadExternalZip :: Test
 testReadExternalZip = TestCase $ do
-  runCommand "/usr/bin/zip -q -9 test-temp/test4.zip zip-archive.cabal Codec/Archive/Zip.hs" >>= waitForProcess
+  _ <- runCommand "/usr/bin/zip -q -9 test-temp/test4.zip zip-archive.cabal Codec/Archive/Zip.hs" >>= waitForProcess
   archive <- toArchive <$> B.readFile "test-temp/test4.zip"
   let files = filesInArchive archive
   assertEqual "for results of filesInArchive" ["zip-archive.cabal", "Codec/Archive/Zip.hs"] files
@@ -50,10 +57,12 @@ testReadExternalZip = TestCase $ do
        Nothing  -> assertFailure "zip-archive.cabal not found in archive"
        Just f   -> assertEqual "for contents of zip-archive.cabal in archive" cabalContents (fromEntry f)
 
+testFromToArchive :: Test
 testFromToArchive = TestCase $ do
   archive <- addFilesToArchive [OptRecursive] emptyArchive ["LICENSE", "Codec"]
   assertEqual "for (toArchive $ fromArchive archive)" archive (toArchive $ fromArchive archive)
 
+testReadWriteEntry :: Test
 testReadWriteEntry = TestCase $ do
   entry <- readEntry [] "zip-archive.cabal"
   setCurrentDirectory "test-temp"
@@ -63,18 +72,21 @@ testReadWriteEntry = TestCase $ do
   let entry'' = entry' { eRelativePath = eRelativePath entry, eLastModified = eLastModified entry }
   assertEqual "for readEntry -> writeEntry -> readEntry" entry entry''
 
+testAddFilesOptions :: Test
 testAddFilesOptions = TestCase $ do
   archive1 <- addFilesToArchive [OptVerbose] emptyArchive ["LICENSE", "Codec"]
   archive2 <- addFilesToArchive [OptRecursive, OptVerbose] archive1 ["LICENSE", "Codec"]
   assertBool "for recursive and nonrecursive addFilesToArchive"
      (length (filesInArchive archive1) < length (filesInArchive archive2))
 
+testDeleteEntries :: Test
 testDeleteEntries = TestCase $ do
   archive1 <- addFilesToArchive [] emptyArchive ["LICENSE", "Codec/"] 
   let archive2 = deleteEntryFromArchive "LICENSE" archive1
   let archive3 = deleteEntryFromArchive "Codec/" archive2
   assertEqual "for deleteFilesFromArchive" emptyArchive archive3
 
+testExtractFiles :: Test
 testExtractFiles = TestCase $ do
   createDirectory "test-temp/dir1"
   createDirectory "test-temp/dir1/dir2"
