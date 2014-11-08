f64af4613e036c423c420770635f908537fd752e
John MacFarlane <jgm@berkeley.edu>
2014-11-08 12:33:23 -0800

    Workaround for `toArchiveOrFail`.
    It is impossible to write this function using the old binary
    interface (< 0.7), which simply raises an error when parsing fails.
    So, when compiled against binary < 0.7, this function will simply
    call `toArchive` and slap on a Right; that is, it won't be safe.
    This is advertised in the haddock comment.
    
    This is ugly, but worth it to allow some form of the library to
    be compiled against older versions of binary/ghc.
    
    Closes #20.

--- src/Codec/Archive/Zip.hs
+++ src/Codec/Archive/Zip.hs
@@ -154,12 +154,19 @@ emptyArchive = Archive
 toArchive :: B.ByteString -> Archive
 toArchive = decode
 
--- | Like 'toArchive', but returns an 'Either' value instead of raising an error
--- if the archive cannot be decoded.
+-- | Like 'toArchive', but returns an 'Either' value instead of raising an
+-- error if the archive cannot be decoded.  NOTE:  This function only
+-- works properly when the library is compiled against binary >= 0.7.
+-- With earlier versions, it will always return a Right value,
+-- raising an error if parsing fails.
 toArchiveOrFail :: B.ByteString -> Either String Archive
+#if MIN_VERSION_binary(0,7,0)
 toArchiveOrFail bs = case decodeOrFail bs of
                            Left (_,_,e)  -> Left e
                            Right (_,_,x) -> Right x
+#else
+toArchiveOrFail bs = Right $ toArchive bs
+#endif
 
 -- | Writes an 'Archive' structure to a raw zip archive (in a lazy bytestring).
 fromArchive :: Archive -> B.ByteString
